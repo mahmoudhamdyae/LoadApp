13c7e0de64a8da286c15b9d1985016f039ee38b0
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2022-12-19 11:23:23 +0200

    Initial

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -2,10 +2,12 @@ package com.udacity
 
 import android.app.NotificationManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.udacity.util.cancelNotifications
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -19,5 +21,9 @@ class DetailActivity : AppCompatActivity() {
             NotificationManager::class.java
         ) as NotificationManager
         notificationManager.cancelNotifications()
+
+        ok_button.setOnClickListener {
+            finish()
+        }
     }
 }
